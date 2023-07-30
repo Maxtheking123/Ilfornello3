22816143593246a3c8e59fefb77e52127e5e1c4d
Maxtheking123 <47282020+Maxtheking123@users.noreply.github.com>
2023-07-30 15:45:01 +0200

    fix: template paths

--- index.py
+++ index.py
@@ -1,31 +1,33 @@
 from flask import Flask, render_template
+import os 
 
+dir_path = os.path.dirname(os.path.realpath(__file__)) + "/"
 app = Flask(__name__)
 
 @app.route('/')
 def index():  # put application's code here
-    return render_template('templates/index.html')
+    return render_template(dir_path + 'templates/index.html')
 
 @app.route('/bar')
 def Bar():
-    return render_template('templates/bar.html')
+    return render_template(dir_path + 'templates/bar.html')
 
 @app.route('/thecave')
 def TheCave():
-    return render_template('templates/thecave.html')
+    return render_template(dir_path + 'templates/thecave.html')
 
 @app.route('/swirland')
 def Swirland():
-    return render_template('templates/swirland.html')
+    return render_template(dir_path + 'templates/swirland.html')
 
 @app.route('/pool')
 def Pool():
-    return render_template('templates/pool.html')
+    return render_template(dir_path + 'templates/pool.html')
 
 @app.route('/projector')
 def Projector():
-    return render_template('templates/projector.html')
+    return render_template(dir_path + 'templates/projector.html')
 
 @app.route('/kitchen')
 def Kitchen():
-    return render_template('templates/kitchen.html')
+    return render_template(dir_path + 'templates/kitchen.html')
